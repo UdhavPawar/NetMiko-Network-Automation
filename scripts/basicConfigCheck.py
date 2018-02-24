88b19478dd084e59fa7fd1b85b71af152b765615
UdhavPawar <upawar78@gmail.com>
2018-02-24 03:59:52 -0600

    Basic Config Check script

---
+++ scripts/basicConfigCheck.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from netmiko import ConnectHandler
+
+iosv_l2_s1 = {
+    'device_type' : 'cisco_ios',
+    'ip' : '192.168.122.72',
+    'username' : 'upawar',
+    'password' : 'cisco'
+}
+
+net_connect = ConnectHandler(**iosv_l2_s1)
+output = net_connect.send_command('show ip int brief')
+print(output)
